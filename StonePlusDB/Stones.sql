9c8bd73340c04ba1acd6ce5a4d80d14227f54faa
John Supina <jsupina32@aim.com>
2018-12-13 17:05:55 -0500

    Add project files.

---
+++ StonePlusDB/Stones.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Stones] (
+    [StoneID] INT           IDENTITY (1, 1) NOT NULL,
+    [Code]    VARCHAR (50) NULL,
+    [Convert]  FLOAT           NULL,
+    PRIMARY KEY CLUSTERED ([StoneID] ASC)
+)
